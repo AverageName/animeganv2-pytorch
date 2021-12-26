de15354dabd82eb3d695871fccd82ef8d5a44ad3
AverageName <max.pakhomov@mail.ru>
2021-12-26 07:53:20 +0000

    Added discriminator architecture

--- model.py
+++ model.py
@@ -107,4 +107,62 @@ class Generator(nn.Module):
 
         out = self.out_layer(out)
         return out
+
+class ConvLayerNormLeakyReLU(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size, 
+                 stride, spatial, spec_norm, norm=None):
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, 
+                  stride, kernel_size // 2, bias=False)
+        
+        if spec_norm:
+            self.conv = nn.utils.spectral_norm(self.conv)
+
+        if norm is not None:
+            self.norm = nn.LayerNorm([out_channels, *spatial])
+        else:
+            self.norm = nn.Identity()
+        
+            
+    def forward(self, x):
+        return F.leaky_relu(self.norm(self.conv(x)), 0.2)
+
+        
+class Discriminator(nn.Module):
+
+    def __init__(self, in_shape, n_dis, spec_norm):
+        super().__init__()
+
+        out_channel = in_shape[0] // 2
+        in_channel = in_shape[0]
+
+        spatial = in_shape[1:]
+
+        self.first_block = ConvLayerNormLeakyReLU(in_channel, out_channel, 3, 1, spatial, spec_norm)
+        in_channel = out_channel
+        self.blocks = []
+
+        for i in range(1, n_dis):
+            self.blocks.append(ConvLayerNormLeakyReLU(in_channel, out_channel * 2, 3, 2, spatial, spec_norm))
+            spatial = [spatial[0] // 2, spatial[1] // 2]
+
+            self.blocks.append(ConvLayerNormLeakyReLU(out_channel * 2, out_channel * 4, 3, 1, spatial, spec_norm, norm='layer'))
+            in_channel = out_channel * 4
+            out_channel = out_channel * 2
+          
+        self.last_block = ConvLayerNormLeakyReLU(in_channel, out_channel * 2, 3, 1, spatial, spec_norm, norm='layer')
+        self.last_conv = nn.Conv2d(out_channel * 2, 1, 3, 1, bias=False)
+        if spec_norm:
+            self.last_conv = nn.utils.spectral_norm(self.last_conv)
+        self.blocks = nn.Sequential(*self.blocks)
+
+    def forward(self, x):
+        out = self.first_block(x)
+        out = self.blocks(out)
+        out = self.last_block(out)
+        out = self.last_conv(out)
+
+        return out
         
